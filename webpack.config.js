39680132b017e2114f17080d4861d34a975ab220
Willy Tjandra <willy.tjandra@gmail.com>
2023-05-08 20:34:36 +1000

    run prettier to fix coding style and format

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,26 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
-    entry: path.resolve(__dirname, 'src/index.js'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[contenthash:8].js',
-        clean: true
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash:8].js',
+    clean: true,
+  },
+  devServer: {
+    port: 3000,
+    static: {
+      directory: path.join(__dirname, 'dist'),
     },
-    devServer: {
-        port: 3000,
-        static: {
-            directory: path.join(__dirname, 'dist')
-        },
-        open: true
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'React App - Local',
-            template: path.resolve(__dirname, '/public/index.html'),
-            inject: 'body'
-        })
-    ]
-};
+    open: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'React App - Local',
+      template: path.resolve(__dirname, '/public/index.html'),
+      inject: 'body',
+    }),
+  ],
+}
