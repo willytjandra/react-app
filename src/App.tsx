cb5dbf48c30b7dd8fbde27240e105a220ace3d90
Willy Tjandra <willy.tjandra@gmail.com>
2023-05-08 21:04:31 +1000

    add support for css
    - install dev dependencies style-loader and css-loader
    - configure both loaders in webpack
    - add basic css for App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
-const App = () => <h1>Welcome to React Application!</h1>
+import styles from './App.module.css'
+
+const App = () => <h1 className={styles.title}>Welcome to React Application!</h1>
 
 export default App
