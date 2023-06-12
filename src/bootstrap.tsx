060d02e5ba4734385a03055237d557a273910fc7
Willy Tjandra <willy.tjandra@gmail.com>
2023-06-12 16:06:15 +1000

    rename app.tsx to App.tsx

--- src/bootstrap.tsx
+++ src/bootstrap.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './app'
+import App from './App'
 
 const container = document.getElementById('root')
 if (container != null) {
